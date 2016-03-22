7cf35a4b263064a7ff00699e0ddea0e625176b5e
Chen Yang <yangc5@mail.uc.edu>
2016-03-22 16:08:55 +0000

    done

--- conf.js
+++ conf.js
@@ -1,5 +1,11 @@
 exports.config = {
-	seleniumAddress: 'http://localhost:4444/wd/hub',
+	seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
 	specs: ['spec/server.js', 'spec/**/*.spec.js'],
+  capabilities: {
+    'browserName': 'phantomjs',
+    'phantomjs.binary.path': require('phantomjs-prebuilt').path,
+    'phantomjs.ghostdriver.cli.args': ['--loglevel=DEBUG']
+  },
+  framework: 'jasmine2',
 	rootElement: '.app'
 };
--- js/app/directives/Counter.js
+++ js/app/directives/Counter.js
@@ -4,12 +4,24 @@ function Counter() {
 			'<div>',
 				'<h3>Counter</h3>',
 				'<div>Click anywhere to increment the counter!</div>',
-				'<div>Current count: {{ count }}</div>',
+				'<div>Current count: {{ ctrl.count }}</div>',
 			'</div>'
 		].join(''),
-		controller: function ($scope) {
-			$scope.count = 0;
-		}
+		controller: function () {
+			this.count = 0;
+		},
+    controllerAs: 'ctrl',
+    require: 'counter',
+    link: function(scope, element, attrs, ctrl){
+      element.on('click', function(){
+        ctrl.count++;
+        scope.$apply();
+      });
+      scope.$on('$destroy', function(){
+        element.off();
+      });
+    }
+
 	}
 }
 
